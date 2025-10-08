1f8cdc8ac661abbbc8399c022ee32c99db7aef0d
onebyte2016 <onebyte2016@gmail.com>
2025-10-08 08:30:23 +0000

    update settings

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party
-    'graphene_django',
+    'graphene-django',
     # Local apps
     'crm',
     'django_filters',
