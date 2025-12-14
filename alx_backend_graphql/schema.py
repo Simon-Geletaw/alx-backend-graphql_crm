43df284ba8f7e2e3e57ae0c6d8c2debfbf49932d
simon geletaw <simongeletaw2@gmail.com>
2025-12-14 14:58:10 -0400

    fix schema

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,8 +1,9 @@
 import graphene
 
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(defualt_value="Hello,GraphQL!")
+
+class Query(CRMQuery, graphene.ObjectType)
+hello = graphene.String(defualt_value="Hello,GraphQL!")
 
 
 schema = graphene.Schema(query=Query)
