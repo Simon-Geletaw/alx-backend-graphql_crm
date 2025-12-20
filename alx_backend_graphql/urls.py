635ffa634743bd863062bad5cfb2f0299eab2bd6
Simon Geletaw <132843442+Simon-Geletaw@users.noreply.github.com>
2025-12-21 02:06:48 +0300

    Fix URL configuration and update Django version in comments

--- alx_backend_graphql/urls.py
+++ alx_backend_graphql/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for alx_back_endgraphql_crm project.
+URL configuration for alx_backend_graphql_crm project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
-
 urlpatterns = [
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)),)
+    path('admin/', admin.site.urls),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
-0
